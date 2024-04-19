92bb9f1e1161d7ae046bb28cacc7109bb5b73af6
sharanyapolu <sharu.polu123@gmail.com>
2024-04-19 15:20:49 +0530

    tic tok toe game

---
+++ index.js
@@ -0,0 +1,28 @@
+var sign ="X";
+function boxClicked(id){
+    if(document.getElementById(id).innerText ==""){
+document.getElementById(id).innerText = sign;
+calculateResult(sign);
+
+if (sign == "X")
+{
+    sign = "O";
+}else{
+    sign = "X";
+}
+}
+function calculateResult(sign){
+    if( document.getElementById("box1").innerText == sign && document.getElementById("box2").innerText == sign && 
+    document.getElementById("box3").innerText == sign || document.getElementById("box4").innerText == sign && document.getElementById("box5").innerText == sign && 
+    document.getElementById("box6").innerText == sign  || document.getElementById("box7").innerText == sign && document.getElementById("box8").innerText == sign && 
+    document.getElementById("box9").innerText == sign  || document.getElementById("box1").innerText == sign && document.getElementById("box4").innerText == sign && 
+    document.getElementById("box7").innerText == sign  || document.getElementById("box2").innerText == sign && document.getElementById("box5").innerText == sign && 
+    document.getElementById("box8").innerText == sign  || document.getElementById("box3").innerText == sign && document.getElementById("box6").innerText == sign && 
+    document.getElementById("box9").innerText == sign  || document.getElementById("box1").innerText == sign && document.getElementById("box5").innerText == sign && 
+    document.getElementById("box9").innerText == sign || document.getElementById("box3").innerText == sign && document.getElementById("box5").innerText == sign && 
+    document.getElementById("box7").innerText == sign    ){
+        alert("Winner is found !!!!!!!!!");
+        location.reload();
+    }
+}
+}
